c9b79ef7b9b1d8c81faaa35a6d2089929a06fd1f
camalzade_elvin <camalzade_elvin@outlook.com>
2019-12-07 03:34:50 +0400

    Add project files.

---
+++ Blog.Data/Concrete/EfCore/BlogContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.Data.Concrete.EfCore
+{
+   public class BlogContext:DbContext
+    {
+        public DbSet<Blog> MyProperty { get; set; }
+    }
+}
---
+++ Blog.WebUI/Concrete/EfCore/BlogContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blog.WebUI.Entities;
+namespace Blog.WebUI.Concrete.EfCore
+{
+    public class BlogContext:DbContext
+    {
+        public BlogContext(DbContextOptions<BlogContext> options):base(options)
+        {
+
+        }
+        public DbSet<Blog.WebUI.Entities.Blog> Blogs { get; set; }
+        public DbSet<Blog.WebUI.Entities.Category> Categories { get; set; }
+        
+    }
+}
