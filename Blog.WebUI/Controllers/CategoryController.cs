ea0c1f4e861a4708c74ae33f54c9864c43bd53b3
camalzade_elvin <camalzade_elvin@outlook.com>
2019-12-11 21:41:09 +0400

    i add category controller

---
+++ Blog.WebUI/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using Blog.WebUI.Abstraction;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.WebUI.Controllers
+{
+    public class CategoryController:Controller
+    {
+        private ICategoryRepository repository;
+
+        public CategoryController(ICategoryRepository repository)
+        {
+            this.repository = repository;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult List()
+        {
+            return View(repository.GetAll());
+        }
+    }
+}
