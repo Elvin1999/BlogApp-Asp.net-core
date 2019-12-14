8d21691ad001c526a3f9488c454e08613081c8d3
camalzade_elvin <camalzade_elvin@outlook.com>
2019-12-14 04:50:03 +0400

    view component

---
+++ Blog.WebUI/ViewComponents/CategoryMenuViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Blog.WebUI.Abstraction;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.WebUI.ViewComponents
+{
+    public class CategoryMenuViewComponent:ViewComponent
+    {
+        private ICategoryRepository _repo;
+
+        public CategoryMenuViewComponent(ICategoryRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(_repo.GetAll());
+        }
+    }
+}
